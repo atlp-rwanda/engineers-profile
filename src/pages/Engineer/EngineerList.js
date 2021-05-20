c1d0144bf946b5d58845ea4d1f21235e39a15208
monehin <e.monehin@live.com>
2021-05-20 08:40:31 +0200

    add spining icon when loading

--- src/pages/Engineer/EngineerList.js
+++ src/pages/Engineer/EngineerList.js
@@ -3,10 +3,11 @@ import qs from 'qs';
 import ListFilter from './ListFilter';
 import ProfileCard from './ProfileCard';
 import { api } from '../../utils';
-import { InfoCircleOutlined } from '@ant-design/icons';
+import { InfoCircleOutlined, LoadingOutlined } from '@ant-design/icons';
 
 const EngineerList = () => {
   const [engineers, setEngineers] = useState([]);
+  const [loading, setLoadState] = useState(true);
 
   useEffect(() => {
     fetchEngineers();
@@ -21,7 +22,9 @@ const EngineerList = () => {
       const res = await api.get(`/users?${query}`);
       const { data } = res;
       setEngineers([...engineers, ...data]);
+      setLoadState(false);
     } catch (err) {
+      setLoadState(false);
       console.log(err);
     }
   };
@@ -29,16 +32,20 @@ const EngineerList = () => {
     <div className='w-full flex flex-col justify-center items-center'>
       <ListFilter />
       <div className='w-4/5 min-h-full mt-10 md:mt-20 flex justify-center '>
-        {engineers.length ? (
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-x-10 gap-y-14'>
-            {engineers.map((data, index) => (
-              <ProfileCard key={data.id} id={index} data={data} />
-            ))}
-          </div>
+        {!loading ? (
+          engineers.length ? (
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-x-10 gap-y-14'>
+              {engineers.map((data, index) => (
+                <ProfileCard key={data.id} id={index} data={data} />
+              ))}
+            </div>
+          ) : (
+            <div className='flex justify-center items-center w-60 rounded-lg p-4 border border-gray-200 gap-x-2'>
+              <InfoCircleOutlined /> No Engineer Data
+            </div>
+          )
         ) : (
-          <div className='flex justify-center items-center w-60 rounded-lg p-4 border border-gray-200 gap-x-2'>
-            <InfoCircleOutlined /> No Engineer Data
-          </div>
+          <LoadingOutlined style={{ fontSize: 24 }} spin />
         )}
       </div>
     </div>
